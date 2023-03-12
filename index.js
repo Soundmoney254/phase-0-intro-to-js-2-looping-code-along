9578eb665fd5abb83297bc797c089f1d7559cd72
Samuel <samnmbuguah@gmail.com>
2023-03-12 23:47:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const nameArray =["Ada", "Brendan", "Ali"];
+const theOccasion = "birthday";
+
+function writeCards(nameArray, theOccasion){
+  const cards =[];
+  for(let e = 0; e < nameArray.length; e++) {
+    cards.push(`Thank you, ${nameArray[e]}, for the wonderful ${theOccasion} gift!`);
+   }
+   return cards;
+  }
+
+  const cards = writeCards(nameArray,theOccasion)
+  console.log(cards)
+
+
+function countDown(inputNum){
+  while (inputNum >= 0) {
+    console.log(inputNum--);
+  }
+}
+
+inputNum = 13
+console.log(countDown(inputNum))
