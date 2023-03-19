de9688f78da1af31bc0429d3a6cb8bdeff5becf5
Samuel <samnmbuguah@gmail.com>
2023-03-19 16:09:05 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const nameArray =["Ada", "Brendan", "Ali"];
 const theOccasion = "birthday";
 
 function writeCards(nameArray, theOccasion){
-  const cards =[];
+  const card =[];
   for(let e = 0; e < nameArray.length; e++) {
-    cards.push(`Thank you, ${nameArray[e]}, for the wonderful ${theOccasion} gift!`);
+    card.push(`Thank you, ${nameArray[e]}, for the wonderful ${theOccasion} gift!`);
    }
-   return cards;
+   return card;
   }
-
-  const cards = writeCards(nameArray,theOccasion)
-  console.log(cards)
+const cards = writeCards(nameArray,theOccasion)
+console.log(cards)
 
 
 function countDown(inputNum){
@@ -19,5 +18,4 @@ function countDown(inputNum){
   }
 }
 
-inputNum = 13
-console.log(countDown(inputNum))
+countDown(5);
